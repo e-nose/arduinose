ff6292914daa753fd2161f3e8ff182256cc24bc6
Emil9999 <emil.sallem@code.berlin>
2022-03-18 12:52:35 +0100

    changed time format

--- serial-script.py
+++ serial-script.py
@@ -1,7 +1,7 @@
 from tokenize import String
 import serial
 import time
-from datetime import date
+from datetime import date,datetime
 import csv
 
 ser = serial.Serial('/dev/ttyACM0')
@@ -10,10 +10,12 @@ ser.flushInput()
 while True:
     try:
         ser_bytes = ser.readline().decode('utf-8')
+        now= datetime.now()
         print(ser_bytes)
         with open(date.today().strftime("%b-%d-%Y"),"a") as f:
             writer = csv.writer(f,delimiter=",")
-            writer.writerow([time.time(),ser_bytes])
+            writer.writerow([('%02d:%02d.%d'%(now.minute,now.second,now.microsecond))[:-4]
+,ser_bytes])
     except Exception as e:
         print(e)
         print("Keyboard Interrupt")
